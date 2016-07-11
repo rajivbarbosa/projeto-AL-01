e7ad3f5eb41b34d828164959dde1d8b39206f771
Rajiv Barbosa Machado de Souza <rajivbarbosa@gmail.com>
2016-07-11 19:09:25 -0300

    commit inicial
    commit inicial contendo apenas divisão da programação.

---
+++ prot.c
@@ -0,0 +1,5 @@
+/* primeira versão do protótipo de um controlador utilizado em ua maquina de lavar industrial*/
+/*Poli Junior Engenharia*/
+/*pre procesamento>>>>>>>neste fase é definido os tempos de operção das etapas */
+/*etapas de lavagem*/
+/*finalização*/
