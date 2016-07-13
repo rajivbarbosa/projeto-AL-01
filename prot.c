3fca91108be1cb4ce429cee6ebe145f1fd8f508e
Rajiv Barbosa Machado de Souza <rajivbarbosa@gmail.com>
2016-07-12 22:52:45 -0300

    Update prot.c
    declarando variáveis

--- prot.c
+++ prot.c
@@ -3,3 +3,17 @@
 /*pre procesamento>>>>>>>neste fase é definido os tempos de operção das etapas */
 /*etapas de lavagem*/
 /*finalização*/
+#define nivel 8
+
+#define molho 4
+#define saida 7
+#define entrada 6
+#define bater 5
+
+long previousMillis = 0;        // Variável de controle do tempo
+long previousMillisbater = 0;        // Variável de controle do tempo de bater
+long redLedInterval = 3000;     // Tempo em ms do molho a ser executado
+long redLedIntervalbater = 2000; // Tempo em ms do bater a ser executado
+int fase;
+boolean moolho;
+boolean baater;
